0c3d66dec100a22a508d8cc8a0b4a340c0ee549f
darrenj31 <dj394@scarletmail.rutgers.edu>
2023-08-12 01:59:55 -0400

    add event listener for buttons to play round

--- ttt.js
+++ ttt.js
@@ -1,4 +1,15 @@
 
+const buttons = document.querySelectorAll("button");
+console.log(buttons);
+
+function playRoundFunc(e){
+    console.log(e.target.textContent);
+    playRound(e.target.textContent, getComputerChoice());
+}
+
+buttons.forEach((button) => button.addEventListener('click', playRoundFunc));
+
+
 function getComputerChoice(){
     const num = Math.floor(Math.random() * 3);
     if (num == 0){
@@ -7,9 +18,9 @@ function getComputerChoice(){
         return "Paper";
     } else {
         return "Scissors";
-    }
+    }s
 }
-s
+
 function playRound(playerSelection, computerSelection){
     const psLowCase = playerSelection.toLowerCase();
 
@@ -25,23 +36,23 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-function game(){
-    let playerScore = 0;
-    let compScore = 0;
-    for (let i = 0; i<5; i++){
-        const ps = prompt("Next move: ");
-        const result = playRound(ps, getComputerChoice());
-        if (result == 1){
-            playerScore++;
-        } else if(result == 0){
-            compScore++;
-        }
-    }
-    if (playerScore>compScore){
-        return(`${"You win "}${playerScore}${" to "}${compScore}`);
-    } else if(playerScore<compScore){
-        return(`${"You lose "}${playerScore}${" to "}${compScore}`);
-    } else{
-        return "Draw";
-    }
-}
+// function game(){
+//     let playerScore = 0;
+//     let compScore = 0;
+//     for (let i = 0; i<5; i++){
+//         const ps = prompt("Next move: ");
+//         const result = playRound(ps, getComputerChoice());
+//         if (result == 1){
+//             playerScore++;
+//         } else if(result == 0){
+//             compScore++;
+//         }
+//     }
+//     if (playerScore>compScore){
+//         return(`${"You win "}${playerScore}${" to "}${compScore}`);
+//     } else if(playerScore<compScore){
+//         return(`${"You lose "}${playerScore}${" to "}${compScore}`);
+//     } else{
+//         return "Draw";
+//     }
+// }
